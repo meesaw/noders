7a4847d5706beb4838feefbaedf7eeebfc56d006
unknown <ifzanrasheed007@gmail.com>
2022-01-05 22:59:43 +0530

    scope

---
+++ Ifzan/scope.js
@@ -0,0 +1,6 @@
+var a=100;
+let b=200;
+const c=300;
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ Ifzan/scope1.js
@@ -0,0 +1,9 @@
+let a=2022;
+abc();
+function abc()
+{
+    let b=1234;
+    console.log(b);
+    console.log(a);
+}
+console.log(b);
